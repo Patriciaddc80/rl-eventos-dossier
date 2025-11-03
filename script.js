bf06e8a1ac22eacc40e7a96fb409211859027ecf
Patricia Díaz <98458936+Patriciaddc80@users.noreply.github.com>
2025-11-03 15:25:01 +0100

    Actualizar pÃ¡gina Servicio de Montaje: ajustar mÃ¡rgenes y tamaÃ±o de tÃ­tulo

--- script.js
+++ script.js
@@ -115,7 +115,7 @@ const pageContents = [
           <div class="montaje-content-box">
             <p>Este servicio se centra en la colocación, distribución y montaje de elementos decorativos, detalles personalizados, centros de mesas, señalización, regalos y otros elementos decorativos en el lugar del evento.</p>
             
-            <h5>Características de este servicio</h5>
+            <h3 style="margin-top: 4%;">Características de este servicio</h3>
             
             <h4>Foco exclusivo en montaje</h4>
             <p>La función principal es la colocación y montaje de todos los detalles y decoración en su lugar correspondiente días o horas antes del evento.</p>
